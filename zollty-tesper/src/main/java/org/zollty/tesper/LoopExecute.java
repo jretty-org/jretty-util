db3701b91eb5ddfae7d24aed67498fe2edde97b4
zollty <zollty@163.com>
2015-06-11 11:15:32 +0000

    update comments

--- zollty-tesper/src/main/java/org/zollty/tesper/LoopExecute.java
+++ zollty-tesper/src/main/java/org/zollty/tesper/LoopExecute.java
@@ -1,19 +1,14 @@
-/*
- * @(#)LoopExecute.java
- * Create by Zollty_Tsow on 2013-12-2 
- * you may find ZollTy at csdn, github, oschina, stackoverflow...
- * e.g. https://github.com/zollty  http://www.cnblogs.com/zollty 
+/* 
+ * Copyright (C) 2013-2015 the original author or authors.
  * 
- * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License").
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
+ * Create by ZollTy on 2013-12-2 (http://blog.zollty.com/, zollty@163.com)
  */
 package org.zollty.tesper;
 
