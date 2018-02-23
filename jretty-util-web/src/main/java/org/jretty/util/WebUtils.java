e6797b479ceff492999d1a6e42201a840fa8c461
zollty <zolltytsou@gmail.com>
2018-02-23 15:50:22 +0800

    add method

--- jretty-util-web/src/main/java/org/jretty/util/WebUtils.java
+++ jretty-util-web/src/main/java/org/jretty/util/WebUtils.java
@@ -17,11 +17,16 @@ import java.io.FileNotFoundException;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.jretty.log.LogFactory;
+import org.jretty.log.Logger;
+
 /**
  * @author zollty
  * @since 2015-3-17
  */
 public class WebUtils {
+    
+    private static Logger logger = LogFactory.getLogger(WebUtils.class);
     
     /**
      * Return the real path of the given path within the web application,
@@ -60,6 +65,46 @@ public class WebUtils {
             return true;
         }
         return false;
+    }
+    
+    /**
+     * 获取IP地址
+     * 
+     * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
+     * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，
+     * X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = null;
+        try {
+            ip = request.getHeader("x-forwarded-for");
+            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (StringUtils.isEmpty(ip) || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+        } catch (Exception e) {
+            logger.error("IPUtils ERROR ", e);
+        }
+        
+//        //使用代理，则获取第一个IP地址
+//        if(StringUtils.isEmpty(ip) && ip.length() > 15) {
+//          if(ip.indexOf(",") > 0) {
+//              ip = ip.substring(0, ip.indexOf(","));
+//          }
+//      }
+        
+        return ip;
     }
 
 }
